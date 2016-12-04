bdfd5167ff42704403454850f534abb9c6c1dafd
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-04 21:34:54 +0000

    adding spinnner items for first spinner

--- app/src/main/java/org/mjeorrett/android/pingpongscorer/AddGameActivity.java
+++ app/src/main/java/org/mjeorrett/android/pingpongscorer/AddGameActivity.java
@@ -3,11 +3,13 @@ package org.mjeorrett.android.pingpongscorer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import org.mjeorrett.android.pingpongscorer.db.Player.PPSPlayer;
 import org.mjeorrett.android.pingpongscorer.db.Player.PPSPlayerServer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,8 +32,18 @@ public class AddGameActivity extends AppCompatActivity {
         setContentView( R.layout.activity_add_edit_game);
 
         List<PPSPlayer> players = PPSPlayerServer.getInstance( this ).getPlayers();
+        List<String> playerNames = new ArrayList<>();
+
+        for ( PPSPlayer player : players ){
+            playerNames.add( player.getNickname() );
+        }
+
+        ArrayAdapter<String> playerNameAdapter = new ArrayAdapter<>( this, android.R.layout.simple_spinner_item, playerNames );
+        playerNameAdapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
 
         mTeamAPlayerASpinner = (Spinner) findViewById( R.id.activity_add_edit_game_team_a_player_a_spinner );
+        mTeamAPlayerASpinner.setAdapter( playerNameAdapter );
+
         mTeamAPlayerBSpinner = (Spinner) findViewById( R.id.activity_add_edit_game_team_a_player_b_spinner );
         mTeamAScoreSpinner = (Spinner) findViewById( R.id.activity_add_edit_game_team_a_score_spinner );
         mTeamBPlayerASpinner = (Spinner) findViewById( R.id.activity_add_edit_game_team_b_player_a_spinner );
