31381599d5282679e44024a99a33bd496ec8e14b
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-04 22:02:06 +0000

    setting up inputs

--- app/src/main/java/org/mjeorrett/android/pingpongscorer/game/AddGameActivity.java
+++ app/src/main/java/org/mjeorrett/android/pingpongscorer/game/AddGameActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import org.mjeorrett.android.pingpongscorer.R;
@@ -23,10 +24,15 @@ public class AddGameActivity extends AppCompatActivity {
 
     private Spinner mTeamAPlayerASpinner;
     private Spinner mTeamAPlayerBSpinner;
-    private Spinner mTeamAScoreSpinner;
+    private EditText mTeamAScoreEditText;
     private Spinner mTeamBPlayerASpinner;
     private Spinner mTeamBPlayerBSpinner;
-    private Spinner mTeamBScoreSpinner;
+    private EditText mTeamBScoreEditText;
+
+    private String mSelectedTeamAPlayerA;
+    private String mSelectedTeamAPlayerB;
+    private String mSelectedTeamBPlayerA;
+    private String mSelectedTeamBPlayerB;
 
     public static Intent newIntent( Context context ) {
 
@@ -41,22 +47,30 @@ public class AddGameActivity extends AppCompatActivity {
         setContentView( R.layout.activity_add_edit_game);
 
         List<PPSPlayer> players = PPSPlayerServer.getInstance( this ).getPlayers();
-        List<String> playerNames = new ArrayList<>();
+        final List<String> playerNames = new ArrayList<>();
 
         for ( PPSPlayer player : players ){
             playerNames.add( player.getNickname() );
         }
 
-        ArrayAdapter<String> playerNameAdapter = new ArrayAdapter<>( this, android.R.layout.simple_spinner_item, playerNames );
-        playerNameAdapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+        ArrayAdapter<String> teamAPlayerAAdaptor = new ArrayAdapter<>( this, android.R.layout.simple_spinner_item, playerNames );
+        teamAPlayerAAdaptor.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
 
         mTeamAPlayerASpinner = (Spinner) findViewById( R.id.activity_add_edit_game_team_a_player_a_spinner );
-        mTeamAPlayerASpinner.setAdapter( playerNameAdapter );
+        mTeamAPlayerASpinner.setAdapter( teamAPlayerAAdaptor );
+
 
         mTeamAPlayerBSpinner = (Spinner) findViewById( R.id.activity_add_edit_game_team_a_player_b_spinner );
-        mTeamAScoreSpinner = (Spinner) findViewById( R.id.activity_add_edit_game_team_a_score_spinner );
+        mTeamAPlayerBSpinner.setAdapter( teamAPlayerAAdaptor );
+
+        mTeamAScoreEditText = (EditText) findViewById( R.id.activity_add_edit_game_team_a_score_edit_text);
+
         mTeamBPlayerASpinner = (Spinner) findViewById( R.id.activity_add_edit_game_team_b_player_a_spinner );
+        mTeamBPlayerASpinner.setAdapter( teamAPlayerAAdaptor );
+
         mTeamBPlayerBSpinner = (Spinner) findViewById( R.id.activity_add_edit_game_team_b_player_b_spinner );
-        mTeamBScoreSpinner = (Spinner) findViewById( R.id.activity_add_edit_game_team_b_score_spinner );
+        mTeamBPlayerBSpinner.setAdapter( teamAPlayerAAdaptor );
+
+        mTeamBScoreEditText = (EditText) findViewById( R.id.activity_add_edit_game_team_b_score_edit_text);
     }
 }
